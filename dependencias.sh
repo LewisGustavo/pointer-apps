66c134a2d70defe307bf01d352907b79741ac2ed
Lewis <145073136+LewisGustavo@users.noreply.github.com>
2023-12-07 22:30:48 -0300

    Create dependencias.sh

---
+++ dependencias.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+sudo apt-get update 
+sudo apt-get install git ansible -y
+
+git clone https://github.com/LewisGustavo/pointer-apps.git /tmp/pointer-apps/
+
+cd /tmp/pointer-apps/
+ansible-playbook pointer-playbook.yml
+
